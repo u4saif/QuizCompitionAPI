78b90e2e097c7513927818bac1d2aa0ab9ad6667
eysaif <u4saif@gmail.com>
2024-02-07 19:18:39 +0530

    gateway serviced Added

--- Quiz/Quiz/src/main/java/com/quiz/services/QuestionClient.java
+++ Quiz/Quiz/src/main/java/com/quiz/services/QuestionClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(url = "http://localhost:8082", value ="Question-Service")
+@FeignClient(name = "QUIZ-SERVICE")
 public interface QuestionClient {
 
     @GetMapping("/question/quiz/{quizId}")
